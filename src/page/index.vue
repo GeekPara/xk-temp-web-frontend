bc70e867cc82f12423b41b78b1891ca02b5e89eb
JupiterJun <hbbmjz@163.com>
2022-01-19 08:09:50 +0000

    2

--- src/page/index.vue
+++ src/page/index.vue
@@ -4,6 +4,7 @@
             <v-col>
                 <h1 class="text-h4">登录</h1>
                 <h2 class="text-h5">使用您的鑫考云账户</h2>
+                <img src="../assets/icon/subscriptions_icon.png">
             </v-col>
         </v-row>
     </v-container>
